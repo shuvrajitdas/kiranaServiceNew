bc2ef004900afed043bdab8e68d179910ba40108
shuvrajitdas <shuvrajitdas203@gmail.com>
2024-01-07 22:25:02 +0530

    Added further improvements

--- src/main/java/com/kiranaservice/kiranaServiceNew/service/TransactionService.java
+++ src/main/java/com/kiranaservice/kiranaServiceNew/service/TransactionService.java
@@ -28,18 +28,6 @@ public class TransactionService {
      * If the originalCurrency is not INR, it creates another convertedAmountInINR for the use of store manager
      * Then saves it
      */
-//    @Transactional
-//    public Transaction recordTransaction(Transaction transaction) {
-//        log.info("Transaction received {}", transaction);
-//        if (!transaction.getOriginalCurrency().equals("INR")) {
-//            log.info("Converting currency to INR");
-//            double amountInInr = currencyConversionService.convertToInr(transaction.getOriginalAmount(), transaction.getOriginalCurrency());
-//            log.info("Converted amount in INR {}", amountInInr);
-//            transaction.setConvertedAmountInINR(amountInInr);
-//        }
-//        return this.transactionRepository.save(transaction);
-//    }
-
     @Transactional
     public Transaction recordTransaction(Transaction transaction) {
         log.info("Transaction received {}", transaction);
@@ -72,12 +60,6 @@ public class TransactionService {
     /**
      * Returns all the transactions grouping them by date
      */
-//    public Map<LocalDate, List<Transaction>> getTransactionsGroupedByDate() {
-//        log.info("Successfull returned all transactions");
-//        List<Transaction> transactions = this.transactionRepository.findAll();
-//        return transactions.stream()
-//                .collect(Collectors.groupingBy(transaction -> transaction.getTimestamp().toLocalDate()));
-//    }
 
     public Map<LocalDate, List<Transaction>> getTransactionsGroupedByDate(String kiranaStoreId) {
         List<Transaction> transactions = this.transactionRepository.findByKiranaStoreId(kiranaStoreId);
